6a818685b70095922259b79aae685803c35267c2
iToussaint <iradukundatoussaint01@gmail.com>
2026-02-06 06:25:18 +0200

    Lab 1 part 9 | Integration challenge

---
+++ part9_integration_challenge.dart
@@ -0,0 +1,22 @@
+// ============= PART 9: INTEGRATION CHALLENGE =============
+
+// Q19: NotificationMixin applied to Student
+mixin NotificationMixin {
+  void notifyRegistration(String studentName, String courseName) {
+    print('Notification: $studentName registered for $courseName.');
+  }
+}
+
+class Student with NotificationMixin {
+  String name;
+  Student(this.name);
+
+  void registerCourse(String courseName) {
+    notifyRegistration(name, courseName);
+  }
+}
+
+void main() {
+  var student = Student('Amina');
+  student.registerCourse('Mathematics');
+}
